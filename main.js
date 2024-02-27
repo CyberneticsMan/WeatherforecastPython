07b9d95c8366c358739f90ab64368192e07e53d7
Mahdiar Giahi <cyberneticsman@gmail.com>
2024-02-27 14:12:26 +0330

    created a front end

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 async function fetchData() {
     const response = await fetch("http://127.0.0.1:5000/forecast/Tehran/temperature");
     const temperature = await response.json();
-    document.getElementById("temp").innerHTML = temperature;
-    
+    document.getElementById("heading").innerHTML = temperature + "° C";
+    const d = new Date();
+    let hour = d.getHours();
+    let minute = d.getMinutes();
+    document.getElementById("hour").innerHTML = hour+":"+minute;
+
   }
