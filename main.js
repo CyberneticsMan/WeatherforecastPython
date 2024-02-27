4e4e71f470ff5150f51b151ed9ff33bec1ec9be8
Mahdiar Giahi <cyberneticsman@gmail.com>
2024-02-27 14:27:31 +0330

    minor changes to clock and title

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 async function fetchData() {
-    const response = await fetch("http://127.0.0.1:5000/forecast/Tehran/temperature");
-    const temperature = await response.json();
-    document.getElementById("heading").innerHTML = temperature + "° C";
-    const d = new Date();
-    let hour = d.getHours();
-    let minute = d.getMinutes();
-    document.getElementById("hour").innerHTML = hour+":"+minute;
+  const d = new Date();
+  let hour = d.getHours();
+  let minute = d.getMinutes();
+  document.getElementById("hour").innerHTML = hour + ":" + minute;
+  const response = await fetch("http://127.0.0.1:5000/forecast/Tehran/temperature");
+  const temperature = await response.json();
+  document.getElementById("heading").innerHTML = temperature + "° C";
 
-  }
+
+}
